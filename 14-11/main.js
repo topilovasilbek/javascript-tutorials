9ce71525da4f0b6bc2ea9cb72731662b8b91d90a
topilovasilbek <asilbektback@gmail.com>
2022-11-17 11:04:02 +0500

    if statement | ternary operator | nullish logical operator

---
+++ 14-11/main.js
@@ -0,0 +1,53 @@
+/* let number = 0; */ // input
+// output: musbat, manfiy, 0
+
+/* if (number > 0) {
+  console.log("musbat");
+} else if (number === 0) {
+  console.log("0");
+} else {
+  console.log("manfiy");
+} */
+
+/* let dayNumber = 100;
+
+if (dayNumber === 1) {
+  console.log("Monday");
+} else if (dayNumber === 2) {
+  console.log("Tuesday");
+} else if (dayNumber === 3) {
+  console.log("Wednesday");
+} else if (dayNumber === 4) {
+  console.log("Thursday");
+} else if (dayNumber === 5) {
+  console.log("Friday");
+} else if (dayNumber === 6) {
+  console.log("Saturday");
+} else if (dayNumber === 7) {
+  console.log("Sunday");
+} else {
+  console.log("Wrong day number");
+}
+ */
+
+// let temp = 15;
+
+// temp > 30
+//   ? console.log("issiq")
+//   : temp > 10
+//   ? console.log("iliq")
+//   : temp > 0
+//   ? console.log("sovuq")
+//   : console.log("juda sovuuuuuuuuuuuuuq");
+
+
+// Nullish ?? - logical operator
+
+// console.log(false || false || false || false || false || false);
+
+// console.log('' ?? 'Asilbek');
+// console.log(1 ?? 2 ?? 0 ?? '1' ?? NaN ?? '0' ?? 'asdasdasd' ?? null ?? undefined);
+// console.log(undefined ?? null ?? 2 ?? 0 ?? '1' ?? NaN ?? '0' ?? 'asdasdasd' ?? null ?? undefined);
+
+Daraja **
+console.log(5 ** 40);
