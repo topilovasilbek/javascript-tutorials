b4baee95f3411f1e7de9fc43d9837ecca30e163d
topilovasilbek <asilbektback@gmail.com>
2022-11-17 11:03:18 +0500

    Type conversion | basic operators

---
+++ 12-11/main.js
@@ -0,0 +1,60 @@
+// -----------------Type Conversions
+console.log(+"10")
+console.log(-"10")
+console.log(5 + +"8");
+console.log(5 * "8");
+console.log("Asilbek" + "Topilov")
+
+/* let number = "10.1231213";
+console.log(number)
+console.log(Number(number))
+console.log(parseInt(number))
+console.log(parseFloat(number)) */
+
+/* let number = 19;
+console.log(number);
+console.log(number.toString()) */
+
+// Convert variables to Boolean
+/* -2, -1, 0, 1, 2
+console.log(true == 1);
+console.log(false == 0);
+true - 1, false - 0
+console.log(Boolean(""))
+console.log("0" == true)
+console.log("0" == false) */
+
+// Basic Operators
+/* 20 / 6 = 3(2)
+console.log(100 % 90); // qoldiq
+console.log((1 + 5) * 2)
+let a = 10;
+console.log(a++);
+console.log(++a);
+a /= 100;
+console.log(a); */
+
+// Comparisions
+/* console.log(10 > 15);
+console.log(10 < 10);
+console.log(10 <= 10);
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(1 == '1')
+console.log(1 === '1')
+console.log('something' == 'something');
+console.log('asdz' > 'asdx');
+ sakdjnaskjdasdsa 324234324mk342k3j4n23#$%^&*())*(&^%$#%^&*<>?><?>.;{}":>":{}<[]:"-=-==-=-") */
+
+// Logical Operators
+/* ! - NOT
+console.log(!false);
+console.log(!(7 > 4)); */
+
+// || - OR - Qo`shish
+/* console.log(false || false || true || false);
+console.log((5 > 3) || (5 > 7) || (5 > 10)); */
+
+// && - AND - Ko`paytirish
+/* console.log(true && true && true && false && true && true);
+console.log((5 > 2) && (3 > 10)); */
