d6399ca139b8acfacef8323d4bae2bb43d349dc3
topilovasilbek <asilbektback@gmail.com>
2022-11-18 22:00:40 +0500

    while, do while and difference between them

---
+++ 18-11/revision.js
@@ -0,0 +1,27 @@
+// While
+
+let i = 0;
+while (i < 5) {
+  console.log(i);
+  i++;
+}
+
+console.log("-----------------");
+
+// Do while
+
+let j = 0;
+do {
+  console.log(j);
+  j++;
+} while (j < 5);
+
+
+let k = 0;
+do {
+  console.log(k);
+  k++;
+} while (k === 5);
+// Here we see 0 in console.
+// Because |do while| works before condition checking
+// That is the difference between |while| and |do while| I know
