1b4a61c0b5e3e1e69f81187508a3720feffde4ea
topilovasilbek <asilbektback@gmail.com>
2022-11-29 19:36:31 +0500

    String | string methods

---
+++ 28-11/main-2.js
@@ -0,0 +1,61 @@
+// Strings
+
+// let str = '';
+// let str = "";
+// let str = ``;
+/* let str = `Assalomu       a
+alaykum
+`; */
+/* let str = "aaaa \n\
+\t bbbb"  */
+// let str = "aaaa \"b\" "
+// console.log(str);
+
+/* let str = "Asilbek"
+console.log(str[4])
+console.log(str.at(-4)) // can get minus param
+console.log(str.charAt(-4)) */
+
+/* let str = "    A@s   ilbek ";
+console.log(str.length); */
+
+/* let str = "Asilbek Topilov";
+console.log(str.toLocaleLowerCase('uz')); */
+
+// let str = "aa";
+// console.log(str.indexOf('a'));
+// console.log(str.lastIndexOf('a'));
+// console.log(str.includes('w'));
+// console.log(str.startsWith('    A'));
+// console.log(str.endsWith('k '));
+// console.log(str.padEnd(10, '>'));
+
+
+/* let str = '            Asilbek     Topilov      ';
+// console.log(str.slice(5, 2));
+// console.log(str.substring(5, 2));
+// console.log(str.substr(5, 2));
+console.log(str);
+console.log(str.trim());
+console.log(str.trimStart());
+console.log(str.trimEnd()); */
+
+// console.log('abcdefgh ij kl'.split(' ', 10))
+
+/* console.log('asilbek'.concat('topilov'));
+let str = 'asilbek';
+str = str.concat('topilov');
+console.log(str); */
+
+/* let str = new String('Asilbek');
+console.log(str.valueOf()); */
+
+/* let str = 'Asilbek';
+str[0] = 'B'
+console.log(str); */
+
+let str = 'Asilbek'
+
+for (char of str) {
+    console.log(char);
+}
