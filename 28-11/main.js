859fce91beea04c59e386a8dbd712ae295dea856
topilovasilbek <asilbektback@gmail.com>
2022-11-29 18:01:00 +0500

    Numbers | Math library | Math methods

---
+++ 28-11/main.js
@@ -0,0 +1,69 @@
+/* const obj = {
+    name: {
+        key: 'Asilbek'
+    }
+}
+console.log(obj.surname?.key) */
+
+// Number: integer and float
+/* let num = '1_000_000';
+console.log(-num); */
+/* let num = 1e6 + 1e3 + 1e0;
+console.log(num); */
+
+/* console.log(0.1 + 0.3 === 0.4)
+console.log(0.1 + 0.2  === 0.3) */
+/* console.log(0.1 + 0.7)
+console.log(Number((0.1 + 0.7).toFixed(1))) */
+// console.log(eval('(1 + 4) ** 2 - 3'));
+
+/* let num = 4;
+console.log(Number(num.toString(2)))
+let num2 = 101;
+console.log(parseInt(num2, 2)); */
+
+// console.log(-0);
+// console.log(0);
+/* let res = Object.is(0, -0); // is like ===
+console.log(-0 === 0);
+console.log(res); */
+
+/* let res = Object.is(NaN, NaN); // is like ===
+console.log(NaN === NaN);
+console.log(res); */
+
+// console.log(Math.PI);
+// console.log(Math.abs(-10)); // modul
+// console.log(Math.floor(1.2));
+// console.log(Math.ceil(3.6));
+// console.log(Math.round(6.5));
+
+
+// console.log(Math.floor(-6.8));
+// console.log(Math.trunc(-6.8));
+
+// console.log(Math.trunc('-6.8'));
+// console.log(parseInt(-6.8));
+
+// console.log(parseInt(Math.random() * 10));
+
+// console.log(Math.min(1, 5, 0, 7, -9, 45));
+// console.log(Math.max('1, 5, 0, 7, -9, 45'));
+
+// console.log(5 ** 5);
+// console.log(Math.pow(5, 5));
+
+// console.log(Math.sign(10));
+
+/* console.log(Math.sqrt(81));
+console.log(Math.sqrt(25));
+console.log(Math.cbrt(625)); */
+
+// console.log(Math.sin(1));
+// console.log(Math.cos(1));
+
+// hypot
+let num1 = 4;
+let num2 = 5;
+console.log(Math.hypot(num1, num2));
+console.log(Math.hypot(num1, num2 - 2));
