2424f075ac987763031c2b4778b25cf0227cdfcc
topilovasilbek <asilbektback@gmail.com>
2022-12-12 18:02:57 +0500

    Date objects | JSON

---
+++ 12-12/main.js
@@ -0,0 +1,39 @@
+// Date objects and JSON
+
+// console.log(Date());
+
+// let date = new Date();
+
+// console.log(new Date(1100000000000));
+// console.log(Date.now());
+// console.log(date.getTime());
+
+// console.log(date);
+// console.log(date.constructor);
+/* console.log(date.getDay()); // Hafta kuni 0-6
+console.log(date.getDate()); // Kun 1-31
+console.log(date.getMonth()); // Oy 0-11
+console.log(date.getFullYear()); */
+
+/* console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+console.log(date.getMilliseconds()); */
+
+// console.log(date.getTimezoneOffset());
+
+/* let obj = {
+  name: "Asilbek",
+  age: 20,
+  data: {
+    firstName: "Muslimbek",
+    lastName: "Ilxomov",
+  },
+};
+// console.log(obj);
+
+let json = JSON.stringify(obj, {}, 4); // convert obj to JSON
+
+let obj2 = JSON.parse(json); // convert JSON to object
+
+console.log(obj2); */
