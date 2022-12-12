16247eb7af63b40af03f6c80b2978676467e8867
topilovasilbek <asilbektback@gmail.com>
2022-12-12 20:00:33 +0500

    new Map() | new Set() | WeakMap | WeakSet

--- 12-12/main.js
+++ 12-12/main.js
@@ -37,3 +37,58 @@ let json = JSON.stringify(obj, {}, 4); // convert obj to JSON
 let obj2 = JSON.parse(json); // convert JSON to object
 
 console.log(obj2); */
+
+/* let obj = {
+  name: "Asilbek",
+  age: 22,
+};
+
+let arr = Object.entries(obj); */
+
+// console.log(arr);
+
+/* let mapObject = new Map(arr);
+mapObject.name = "Asilbek 2";
+
+mapObject.set("status", "developer"); */
+// console.log(mapObject);
+
+// mapObject.delete("status")
+// mapObject.clear()
+// console.log(Array.from(mapObject));
+// console.log(Object.fromEntries(mapObject));
+// console.log(mapObject.get('status'));
+
+// mapObject.forEach((item) => console.log(item));
+
+/* console.log(mapObject.entries()); // map Entries
+console.log(Object.entries(mapObject)); //  simple object key&values
+console.log(mapObject.size); */
+
+/* let set = new Set([3, 0, 1, 2, 3, 3, 3, 0]);
+
+console.log(set); */
+
+/* let obj = { name: "Asilbek" };
+
+obj = null; */
+
+/* let obj = {};
+let weakMap = new WeakMap();
+weakMap.set(obj, "Asilbek");
+
+console.log(weakMap.get(obj)); */
+
+// console.log(new WeakSet());
+
+/* let obj = {
+  null: 1,
+};
+
+console.log(obj.null); */
+
+let mapObject = new Map();
+
+mapObject.set({}, 1);
+
+console.log(mapObject.get(null));
