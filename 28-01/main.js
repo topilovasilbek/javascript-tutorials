b6563a1df02129a9680857a7a69b9e57695caaee
topilovasilbek <asilbektback@gmail.com>
2023-01-30 11:32:28 +0500

    What is API | Fetch | restful API

---
+++ 28-01/main.js
@@ -0,0 +1,44 @@
+// Fetch - olib kelish
+// CRUD - Create Read Update Delete
+// Fetch - POST, GET, PUT, DELETE
+
+/* function getData() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((res) => console.log(res));
+}
+getData(); */
+
+/* function postData() {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      name: "Asilbek",
+    }),
+  })
+    .then((res) => res.json())
+}
+postData(); */
+
+/* function putData() {
+  fetch("https://jsonplaceholder.typicode.com/posts/12", {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      name: "Asilbek",
+    }),
+  }).then((res) => res.json());
+}
+putData(); */
+
+/* function deleteData() {
+  fetch("https://jsonplaceholder.typicode.com/posts/12", {
+    method: "DELETE",
+  })
+}
+deleteData(); */
